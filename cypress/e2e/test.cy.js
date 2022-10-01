83a582c17ef21a3b3449072f5e5e4d31c0db25ba
rohanshrivastava15 <rohan@elucidateai.com>
2022-10-02 00:06:57 +0530

    asdad

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,5 +3,5 @@
   it('login', () => {
 
     cy.visit("https://docs.cypress.io/guides/guides/command-line#cypress-run");
-
+    cy.visit("https://docs.cypress.io/guides/guides/command-line#cypress-run");
   })
